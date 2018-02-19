8b2c0a6564faac289412580662eb3990fd394f38
Richard <yuukifushimi@gmail.com>
2018-02-19 14:14:42 -0500

    p

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
+rm -fr bin/*
 rm -fr build/*
 truffle compile
 truffle migrate --reset --network testrpc
-rm -fr bin/*
 solc contracts/PublicOffering.sol --abi --bin --overwrite -o bin/
 solc contracts/SmartKey.sol --abi --bin --overwrite -o bin/
 solc contracts/Database.sol --abi --bin --overwrite -o bin/
--- contracts/PublicOffering.sol
+++ contracts/PublicOffering.sol
@@ -9,11 +9,11 @@ import "./admin/Administered.sol";
  */
 contract PublicOffering is CappedCrowdsale {
 
-  function PublicOffering(SmartKey _token, uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, uint256 _mintedTokens, address[] adminAddress)
+  function PublicOffering(SmartKey _token, uint256 _startTime, uint256 _endTime, uint256 _rate, uint256 _cap, address[] adminAddress)
   public
   Administered(adminAddress)
   CappedCrowdsale(_cap, adminAddress)
-  Crowdsale(_token, _startTime, _endTime, _rate, _mintedTokens, adminAddress)
+  Crowdsale(_token, _startTime, _endTime, _rate, adminAddress)
   {
     
   }
