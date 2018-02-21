a4e98e5acd05853a025c643a3d788945359ce91e
Richard <yuukifushimi@gmail.com>
2018-02-21 10:12:27 -0500

    local changes

--- contracts/CatalogueItem.sol
+++ contracts/CatalogueItem.sol
@@ -8,12 +8,16 @@ pragma solidity ^0.4.11; //We have to specify what version of the compiler this 
 
 contract CatalogueItem is Administered, Key {
        
-  SmartKey smartKey;
+  //PAS 212:2016
   string href;
   MetaData[] itemMetadata;
   mapping (string => CatalogueItem) public catItemData; 
   mapping (string => MetaData) public itemMetaData; 
+  //PAS 212:2016
   
+  SmartKey smartKey;
+  CatalogueItem public parentCatalogueItem;
+  CatalogueItem[] public childCatalogueItem;
   event CatItemMetaData(address indexed user, MetaData indexed catItemMetaData);
 
   function CatalogueItem(SmartKey _smartKey, address[] adminAddress) 
