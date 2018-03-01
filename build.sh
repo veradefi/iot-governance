05f005b5b45059033476683cc2deb8a56d84d8e4
Richard <yuukifushimi@gmail.com>
2018-03-01 12:46:58 -0500

    p

--- build.sh
+++ build.sh
@@ -4,7 +4,6 @@ truffle compile
 truffle migrate --reset --network testrpc
 solc contracts/PublicOffering.sol --abi --bin --overwrite -o bin/
 solc contracts/SmartKey.sol --abi --bin --overwrite -o bin/
-solc contracts/GraphBase.sol --abi --bin --overwrite -o bin/
 solc contracts/GraphNode.sol --abi --bin --overwrite -o bin/
 solc contracts/GraphRoot.sol --abi --bin --overwrite -o bin/
 solc contracts/Catalogue.sol --abi --bin --overwrite -o bin/
