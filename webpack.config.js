ce9994e7d4266b17a8385a3195090bb5c641833a
Richard <yuukifushimi@gmail.com>
2018-03-05 10:26:39 -0500

    p

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,48 @@
-const webpack = require('webpack')
 const path = require('path');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
-      entry: [
-         'babel-polyfill',
-	'./app/javascripts/app.js'
+  entry:  [
+         //'babel-polyfill',
+         './app/javascripts/app.js'
       ],
-      output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.js',
-      },
-      plugins: [
-        // Copy our app's index.html to the build folder.
-        new CopyWebpackPlugin([
-          { from: './app/index.html', to: "index.html" },
-          { from: './app/pas212.html', to: "pas212.html" }
-        ]),
-        //new webpack.HotModuleReplacementPlugin(),
-      
-      ],
-	  node: {
-		    fs: 'empty'
-	  },
-      module: {
-        rules: [
-          {
-           test: /\.css$/,
-           use: [ 'style-loader', 'css-loader' ]
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'app.js'
+  },
+  plugins: [
+    // Copy our app's index.html to the build folder.
+    new CopyWebpackPlugin([
+      { from: './app/index.html', to: "index.html" },
+      { from: './app/pas212.html', to: "pas212.html" }
+    ])
+  ],
+  node: {
+                    fs: 'empty'
           },
-          {
+  module: {
+    rules: [
+      {
+       test: /\.css$/,
+       use: [ 'style-loader', 'css-loader' ]
+      },
+      {
             test: /\.js$/,
             exclude: /(node_modules|bower_components)/,
             use: {
               loader: 'babel-loader',
               options: {
-		      presets: ['@babel/preset-env','@babel/preset-react'],
-		      plugins: [
-			      "@babel/transform-arrow-functions",
-			      "@babel/transform-classes",
-		      ],
-		      "compact": true
-
+                      presets: ['@babel/preset-env'],
+        	      plugins: ["@babel/plugin-transform-runtime", {
+				//"helpers": true,
+      				//"polyfill": true,
+      				//"regenerator": true,
+      				//"moduleName": "@babel/runtime"
+ 		      }],
+		      //"compress":true
               }
             }
-          }
-        ],
-        loaders: [
-          { test: /\.json$/, use: 'json-loader' }
-        ]
-       }
- }
+      }
+    ]
+  }
+}
