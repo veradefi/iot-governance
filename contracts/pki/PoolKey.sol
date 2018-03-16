6882723a383b3e969946cd56eb9ec5b26b73aa3e
Richard <yuukifushimi@gmail.com>
2018-03-16 15:24:59 -0400

    p

--- contracts/pki/PoolKey.sol
+++ contracts/pki/PoolKey.sol
@@ -5,8 +5,9 @@ import '../admin/Whitelisted.sol';
 
 contract PoolKey is Key, Whitelisted 
 {
-
+    
     using SafeMath for uint256;
+    bool has_whitelist;
     address[] public members;
     mapping (address => uint256) public isMember;
     mapping (address => uint256) public received;
@@ -28,8 +29,8 @@ contract PoolKey is Key, Whitelisted
     
     mapping (address => transaction[]) public transactions;
     
-    function PoolKey(address _poolVault, address _beneficiary, uint256 _max_contrib, uint256 _max_per_contrib, uint256 _min_per_contrib, address[] _admins, address[] _whitelist, uint256 _fee) 
-    Whitelisted(_admins, _whitelist) 
+    function PoolKey(address _poolVault, address _beneficiary, uint256 _max_contrib, uint256 _max_per_contrib, uint256 _min_per_contrib, address[] _admins, bool _has_whitelist, uint256 _fee) 
+    Whitelisted(_admins, _admins) 
     Key(_beneficiary) 
     public
     {
@@ -40,19 +41,12 @@ contract PoolKey is Key, Whitelisted
       max_per_contrib=_max_per_contrib;
       min_per_contrib=_min_per_contrib;
       contrib_amount=0;
+      has_whitelist=_has_whitelist;
       
       for (uint k=0; k < _admins.length; k++) {
           members.push(_admins[k]);
       }
-      
-
-      for (uint i=0; i < _whitelist.length; i++) {
-          if (!isAdmin[_whitelist[i]]) {
-              members.push(_whitelist[i]);
-          }
-      }
-      
-    
+          
     }
     
     function getMembers() 
@@ -70,7 +64,7 @@ contract PoolKey is Key, Whitelisted
     
         uint256 weiAmount = msg.value;
         if (weiAmount >= min_per_contrib && isMember[msg.sender] <= max_per_contrib) {
-            if (whitelist.length == 0 || isWhitelisted[msg.sender]) {
+            if (!has_whitelist || isWhitelisted[msg.sender]) {
                 uint256 fee1=weiAmount.div(200); 
                 uint256 fee2=weiAmount.div(fee); 
                 uint256 distAmount = weiAmount.sub(fee1).sub(fee2);
