6d66825a45d2c4052ab2bf01ac1ceadfffc82983
Richard <yuukifushimi@gmail.com>
2018-03-16 06:51:59 -0400

    p

--- contracts/pki/PoolKey.sol
+++ contracts/pki/PoolKey.sol
@@ -7,14 +7,16 @@ contract PoolKey is Key, Whitelisted
 {
 
     using SafeMath for uint256;
-    address[] members;
-    mapping (address => bool) public isMember;
+    address[] public members;
+    mapping (address => uint) public isMember;
     address poolVault;
     address beneficiary;
-    uint max_contrib;
-    uint max_per_contrib;
-    uint min_per_contrib;
-    uint fee;
+    uint public contrib_amount;
+    
+    uint public max_contrib;
+    uint public max_per_contrib;
+    uint public min_per_contrib;
+    uint public fee;
     
     
     function PoolKey(address _poolVault, address _beneficiary, uint _max_contrib, uint _max_per_contrib, uint _min_per_contrib, address[] _admins, address[] _whitelist, uint _fee) 
@@ -28,20 +30,28 @@ contract PoolKey is Key, Whitelisted
       max_contrib=_max_contrib;
       max_per_contrib=_max_per_contrib;
       min_per_contrib=_min_per_contrib;
-      
+      contrib_amount=0;
       
       for (uint k=0; k < _admins.length; k++) {
           members.push(_admins[k]);
-          isMember[_admins[k]]=true;
       }
       
 
       for (uint i=0; i < _whitelist.length; i++) {
-          members.push(_whitelist[i]);
-          isMember[_whitelist[i]]=true;
+          if (!isAdmin[_whitelist[i]]) {
+              members.push(_whitelist[i]);
+          }
       }
       
     
+    }
+    
+    function getMembers() 
+    public
+    constant 
+    returns (address[]) 
+    {
+            return members;
     }
     
     // fallback function can be used to distribute tokens
@@ -51,23 +61,32 @@ contract PoolKey is Key, Whitelisted
     {
     
         uint256 weiAmount = msg.value;
-        uint256 fee1=weiAmount.div(200); 
-        uint256 fee2=weiAmount.div(fee); 
-        uint256 distAmount = weiAmount.sub(fee1).sub(fee2);
-        distAmount = distAmount.div(members.length);
-        
-        poolVault.transfer(fee1);
-        vault.transfer(fee2);
-        
-        if (!isMember[msg.sender]) {
-            isMember[msg.sender]=true;
-        }
-        
-        for (uint i=0; i < members.length; i++) 
-        {
-            members[i].transfer(distAmount);
-        }
-        
+        if (weiAmount >= min_per_contrib && weiAmount <= max_per_contrib) {
+            if (whitelist.length == 0 || isWhitelisted[msg.sender]) {
+                uint256 fee1=weiAmount.div(200); 
+                uint256 fee2=weiAmount.div(fee); 
+                uint256 distAmount = weiAmount.sub(fee1).sub(fee2);
+                distAmount = distAmount.div(members.length);
+                
+                poolVault.transfer(fee1);
+                vault.transfer(fee2);
+                
+                
+                isMember[msg.sender] = isMember[msg.sender].add(weiAmount);
+                contrib_amount=contrib_amount.add(weiAmount);
+            
+                for (uint i=0; i < members.length; i++) 
+                {
+                    members[i].transfer(distAmount);
+                }
+                
+                
+                if (isMember[msg.sender] == weiAmount) {
+                    members.push(msg.sender);
+                }
+                
+            }
+        }        
     }
     
 }
