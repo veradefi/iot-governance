8b54fef5d8478c6f0b3ec400e288a87e35b89fe2
Richard <yuukifushimi@gmail.com>
2018-04-11 07:25:36 -0400

    p

--- build_rinkeby.sh
+++ build_rinkeby.sh
@@ -6,10 +6,6 @@ truffle compile
 truffle migrate --reset --network rinkeby
 sh build_solcjs.sh
 
-sh build_app.sh
-python rinkeby/test_hypercat.py
-python rinkeby/test_pool.py
-cp -pR build/app.js server/browser-tools/htdocs/js
-cp -pR build/contracts server/browser-tools/htdocs/js/
+sh build_rinkeby_data.sh
 
 
