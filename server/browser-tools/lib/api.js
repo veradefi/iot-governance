8d1e87cdbdffd5c2248aa6f6119bf469ec8c7a0f
Richard <yuukifushimi@gmail.com>
2018-04-17 15:19:07 -0400

    p

--- server/browser-tools/lib/api.js
+++ server/browser-tools/lib/api.js
@@ -33,18 +33,141 @@ var express_options = {
 
 var app = express();
 
+app.set('view engine', 'ejs');
+
+
+
+function get_nav() {
+    var nav_menu = [
+      {
+        href: 'pool.html',
+        name: 'Smart Pool Key',
+        selected: false,
+      },
+      {
+        href: 'key.html',
+        name: 'Smart Key',
+        selected: false,
+      },
+      {
+        href: 'browser.html',
+        name: 'Browser & Editor',
+        selected: false,
+      },
+      {
+        href: 'explorer.html',
+        name: 'Explorer',
+        selected: false,
+      },
+      {
+        href: 'crawler.html',
+        name: 'Crawler',
+        selected: false,
+      },
+      {
+        href: 'map.html',
+        name: 'Map',
+        selected: false,
+      }
+    ]    
+    return nav_menu;
+}
+
+app.get('/', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[1]['selected']=true;
+  res.render('key', { nav:render_nav })
+})
+
+app.get('/index.html', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[1]['selected']=true;
+  res.render('key', { nav:render_nav })
+})
+
+app.get('/pool', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[0]['selected']=true;
+  res.render('pool', { nav:render_nav })
+})
+
+app.get('/pool.html', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[0]['selected']=true;
+  res.render('pool', { nav:render_nav })
+})
+
+app.get('/key', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[1]['selected']=true;
+  res.render('key', { nav:render_nav })
+})
+
+app.get('/key.html', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[1]['selected']=true;
+  res.render('key', { nav:render_nav })
+})
+
+app.get('/browser', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[2]['selected']=true;
+  res.render('browser', { nav:render_nav })
+})
+
+app.get('/browser.html', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[2]['selected']=true;
+  res.render('browser', { nav:render_nav })
+})
+
+app.get('/explorer', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[3]['selected']=true;
+  res.render('explorer', { nav:render_nav })
+})
+
+app.get('/explorer.html', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[3]['selected']=true;
+  res.render('explorer', { nav:render_nav })
+})
+
+app.get('/crawler', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[4]['selected']=true;
+  res.render('crawler', { nav:render_nav })
+})
+
+app.get('/crawler.html', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[4]['selected']=true;
+  res.render('crawler', { nav:render_nav })
+})
+
+app.get('/map', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[5]['selected']=true;
+  res.render('map', { nav:render_nav })
+})
+
+app.get('/map.html', (req, res) => {
+  var render_nav=get_nav();
+  render_nav[5]['selected']=true;
+  res.render('map', { nav:render_nav })
+})
+
 app.configure(function () {
     app.set('port', config.port);
     app.use(express.logger('dev'));
     app.use(express.bodyParser()),
-    app.use(express.static(path.join(__dirname, config.htdocs), {index:'index.html'})),
+    app.use(express.static(path.join(__dirname, config.htdocs), {index:'/'})),
     app.use(express.directory(path.join(__dirname, config.htdocs)));
 });
 
 app.get('/fetch', fetcher.fetch);           // web proxy
 app.post('/post', fetcher.post);           // web proxy
 app.del('/del', fetcher.del);           // web proxy
-app.get('/listexamples', dir.listexamples); // list examples dir to populate GUI menu
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Express server listening on port " + app.get('port'));
