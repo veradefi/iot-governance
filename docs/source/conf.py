9ef3df7b419e4ca99799b08eab6d5eb12ea6b024
Richard <yuukifushimi@gmail.com>
2018-04-25 14:26:10 -0400

    p

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -95,23 +95,29 @@ todo_include_todos = False
 #    "rightsidebar": "false",
 #    "relbarbgcolor": "black"
 #}
-html_theme = "sphinx_rtd_theme"
-html_theme_path = ["_themes", ]
-html_theme_options = {
-    'canonical_url': '',
-    'analytics_id': '',
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    # Toc options
-    'collapse_navigation': False,
-    'sticky_navigation': False,
-    'navigation_depth': 5,
-    'includehidden': True,
-    'titles_only': False
-}
+html_theme = 'default'
+try:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme_options = {
+        'canonical_url': '',
+        'analytics_id': '',
+        'logo_only': False,
+        'display_version': True,
+        'prev_next_buttons_location': 'bottom',
+        'style_external_links': False,
+        'vcs_pageview_mode': '',
+        # Toc options
+        'collapse_navigation': False,
+        'sticky_navigation': False,
+        'navigation_depth': 5,
+        'includehidden': True,
+        'titles_only': False
+    }
+except ImportError:
+    pass
+
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
