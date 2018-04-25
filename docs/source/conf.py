532eaf1d6e1d63b852ed84c24e08176c0c81ddfc
Richard <yuukifushimi@gmail.com>
2018-04-25 14:21:15 -0400

    p

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,7 @@ def setup(app):
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinxcontrib.httpdomain']
+extensions = ['sphinxcontrib.httpdomain']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
