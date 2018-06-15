6b47432cbfc2879bc9285cf6a1e9d837d17d0f57
Richard <yuukifushimi@gmail.com>
2018-06-15 11:17:24 +0800

    p

--- rinkeby/test_pool_manual.py
+++ rinkeby/test_pool_manual.py
@@ -25,7 +25,7 @@ def getContract(item, network, address=None, prefix=""):
 network='4'
 port='8666'
 #web3 = Web3(IPCProvider("~/.ethereum/rinkeby/geth.ipc"))
-web3 = Web3(HTTPProvider('http://localhost:' + port ))
+web3 = Web3(HTTPProvider('http://35.165.47.77:' + port ))
 #web3 = Web3(HTTPProvider('https://rinkeby.infura.io/8BNRVVlo2wy7YaOLcKCR'))
 address2=web3.eth.coinbase
 address=web3.eth.accounts[1]
