9330bd3bc4fbeb6c15266e161d16c0d3b6602bce
Richard <yuukifushimi@gmail.com>
2018-07-20 22:35:22 +0800

    p

--- server/browser/build_rinkeby_data.sh
+++ server/browser/build_rinkeby_data.sh
@@ -3,9 +3,9 @@ PATH=./node_modules/.bin:$PATH
 export PATH
 
 sh build_app.sh
-python rinkeby/restore_hypercat.py
+python rinkeby/test_restore_hypercat.py
 #python rinkeby/test_pool.py
-cp -pR build/app.js server/browser-tools/htdocs/js
-cp -pR build/contracts server/browser-tools/htdocs/js/
+#cp -pR build/app.js server/browser-tools/htdocs/js
+#cp -pR build/contracts server/browser-tools/htdocs/js/
 
 
