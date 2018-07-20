5ee858735925a928832cdff94cb23dc1657c8395
Richard <yuukifushimi@gmail.com>
2018-07-20 20:46:24 +0800

    local changes

--- server/browser/server/index.py
+++ server/browser/server/index.py
@@ -661,6 +661,31 @@ def metaSearch(request, data):
     
     try:
         try:
+            q = request.args.get('q')
+            if q:
+                filtered_items=[]
+                items=data['items']
+                for item in items:
+                    try:
+                        found=False
+                        if (item['href'] and re.search(q, item['href'])):
+                            found=True
+                        metas=item['item-metadata']
+                        for meta in metas:
+                            if (meta['rel'] and re.search(q, meta['rel'])) or \
+                               (meta['val'] and re.search(q, meta['val'])):
+                                found=True
+                        if found:
+                            filtered_items.append(item)
+                    except Exception as e:
+                        print (e)
+                        traceback.print_exc()
+                data['items']=filtered_items
+        except Exception as e:
+            print (e)
+            traceback.print_exc()
+
+        try:
             rel = request.args.get('rel')
             val = request.args.get('val')
             if rel or val:
