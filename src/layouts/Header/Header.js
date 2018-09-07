8509ee30bacc6a10bed8e9198ac4deb16753d1b9
Richard <yuukifushimi@gmail.com>
2018-09-07 22:33:51 +0800

    p

--- src/layouts/Header/Header.js
+++ src/layouts/Header/Header.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
+import Hidden from '@material-ui/core/Hidden';
 
 export default class Header extends Component {
   render() {
     return(
+        <Hidden smDown>
+
             <div className={"m-portlet__head"} style={{backgroundColor: "#ffffff", boxShadow: "0 2px 7px 0 rgba(160, 160, 160, 0.5)",
             boxShadow: "0 2px 7px 0 rgba(160, 160, 160, 0.5)",
             border: "solid 1px #e7e7e7"
@@ -25,6 +28,7 @@ export default class Header extends Component {
                     </div>
                     <div style={{clear: "both"}}></div>​
              </div>
+        </Hidden>
     )
   }
 }
