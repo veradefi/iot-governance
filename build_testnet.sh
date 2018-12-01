a7d5c94781db494cefb057bef021a349d7767e0d
Mike <aarbache@gmail.com>
2018-12-01 00:25:53 -0800

    fix quick build

--- build_testnet.sh
+++ build_testnet.sh
@@ -9,15 +9,22 @@ rm -fr build/*
 rm -fr src/solc/contracts/*
 truffle compile
 truffle migrate --reset --network development
+
+if ! [ -d  src/solc/contracts/ ] 
+then
+    mkdir -p src/solc/contracts/
+fi
+
 cp -pR build/contracts/* src/solc/contracts/
+
 sh build_solcjs.sh
 case $1 in
-	bg)
-		python3 server/index_testnet.py &
-		;;
-	*)
-		python3 server/index_testnet.py 
-		;;
+    bg)
+        python3 server/index_testnet.py &
+        ;;
+    *)
+        python3 server/index_testnet.py 
+        ;;
 esac
 #python3 testnet/restore_hypercat.py
 #python3 testnet/test_smartkey.py
