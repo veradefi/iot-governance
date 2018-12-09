762d0165f1ef4fc9f7f5eba3bb90c11707d2bc2b
Richard <yuukifushimi@gmail.com>
2018-12-09 03:25:07 -0500

    yarn ganache testnet build

--- build_testnet.sh
+++ build_testnet.sh
@@ -4,15 +4,19 @@ PATH=./node_modules/.bin:$PATH
 export PATH
 killall -9 node
 killall -9 python3
-yarn
+npm install yarn
+chmod +x node_modules/.bin/yarn
+node_modules/.bin/yarn
 tar -xvf web3providerfix_local.tar
-ganache-cli -p 9545 -i 4447 -e 1000 &
+chmod +x node_modules/.bin/ganache-cli
+./node_modules/.bin/ganache-cli -p 9545 -i 4447 -e 1000 &
 
 rm -fr bin/*
 rm -fr build/*
 rm -fr src/solc/contracts/*
-truffle compile
-truffle migrate --reset --network development
+chmod +x node_modules/.bin/truffle
+./node_modules/.bin/truffle compile
+./node_modules/.bin/truffle migrate --reset --network development
 
 if ! [ -d  src/solc/contracts/ ] 
 then
